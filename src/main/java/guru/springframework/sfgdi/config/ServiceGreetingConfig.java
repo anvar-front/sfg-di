797ccd1e4f2413349c47be4471deb1b541918a5c
anvar <basic.num7@gmail.com>
2023-01-31 23:05:05 +0600

    added PetServiceFactory

--- src/main/java/guru/springframework/sfgdi/config/ServiceGreetingConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/ServiceGreetingConfig.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgdi.config;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,15 +10,37 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class ServiceGreetingConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
+    }
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile("cat")
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("cat");
+    }
+
     @Primary
     @Bean
     PrimaryGreetingService primaryGreetingService(){
         return new PrimaryGreetingService();
     }
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+    }
+
     @Profile({"EN", "default"})
     @Bean
-    I18nEnglishGreetingService i18nService(){
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
     @Profile("ES")
     @Bean("i18nService")
